78477746739d4655a63a9e3376beb0fedd1c0c37
HwangChangyeon <ckddusl61@Naver.com>
2017-06-26 16:59:12 +0900

    Hello.java modified GitHub

--- Hello.java
+++ Hello.java
@@ -1,1 +1,9 @@
-﻿salkdlqkwlke
+//For command line gitgit
+public class Hello{
+  public static main(String[] args){
+    System.out.println("HelloWorld");
+    System.out.println("HelloWorld2");
+    System.out.println("Tell your World");
+    System.out.println("Tell his World");
+  }
+}
